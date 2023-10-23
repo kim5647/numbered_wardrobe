5c245929d3cf7d0797b84b6e72ee679c06ab58b4
kim5647 <75043215+kim5647@users.noreply.github.com>
2023-10-23 22:34:19 +0300

    1commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -18,48 +18,64 @@ class MyHomePage extends StatelessWidget {
       
             children: <Widget>[
              Expanded(
+                flex: 3,
                 child: 
                 Row(                
                 children: <Widget>[
-                  Expanded(child: 
+                  Expanded(
+                    child: 
                   Container(
                     color: Colors.black, 
-                    margin: EdgeInsets.only(left: 8, top: 8, right: 4, bottom: 4),
-                    
+                    margin: const EdgeInsets.only(left: 8, top: 8, right: 4, bottom: 4),
+                    child: const Center(child: Text(style: TextStyle(color: Color.fromRGBO(245, 255, 151, 0.965), fontSize: 60.0), '1'),)
                     ),
                     ),
-                  Expanded(child: Container(color: Colors.yellow, margin: EdgeInsets.only(left: 4, top: 8, right: 8, bottom: 4),)),
+                  Expanded(child: Container(color: Colors.yellow, margin: const EdgeInsets.only(left: 4, top: 8, right: 8, bottom: 4),
+                  child: const Center(child: Text(style: TextStyle(color: Color.fromRGBO(216, 0, 43, 0.965), fontSize: 60.0), '1'),)
+                  )),
                   ],
-                  ),              
-                flex: 3,
+                  ),
             ),
             Expanded(
+                flex:1,
                 child: Row(
                   children: [
-                    Expanded(child: Container(color: Colors.red, margin: EdgeInsets.only(left: 8, top: 4, right: 4, bottom: 4),)),
-                    Expanded(child: Container(color: Colors.orange, margin: EdgeInsets.only(left: 4, top: 4, right: 8, bottom: 4),)),
+                    const Center(child: Text('1'),),
+                    Expanded(child: Container(color: Colors.red, margin: const EdgeInsets.only(left: 8, top: 4, right: 4, bottom: 4),
+                    child: const Center(child: Text(style: TextStyle(color: Color.fromRGBO(255, 253, 160, 0.965), fontSize: 60.0), '1'),)
+                    )),
+                    Expanded(child: Container(color: Colors.orange, margin: const EdgeInsets.only(left: 4, top: 4, right: 8, bottom: 4),
+                    child: const Center(child: Text(style: TextStyle(color: Color.fromRGBO(132, 0, 0, 0.965), fontSize: 60.0), '1'),)
+                    )),
                   ],
-                ),
-                flex:1
+                )
             ),
              Expanded(
+                flex:1,
                 child: Row(
                   children: [
-                    Expanded(child: Container(color: Colors.indigoAccent, margin: EdgeInsets.only(left: 8, top: 4, right: 4, bottom: 4),)),
-                    Expanded(child: Container(color: Colors.cyan, margin: EdgeInsets.only(left: 4, top: 4, right: 8, bottom: 4),)),
+                    Expanded(child: Container(color: Colors.indigoAccent, margin: const EdgeInsets.only(left: 8, top: 4, right: 4, bottom: 4),
+                    child: const Center(child: Text(style: TextStyle(color: Color.fromRGBO(0, 0, 0, 0.965), fontSize: 60.0), '1'),)
+                    )),
+                    Expanded(child: Container(color: Colors.cyan, margin: const EdgeInsets.only(left: 4, top: 4, right: 8, bottom: 4),
+                    child: const Center(child: Text(style: TextStyle(color: Color.fromRGBO(86, 17, 85, 0.965), fontSize: 60.0), '1'),)
+                    )),
                   ],
-                ),
-                flex:1
+                )
             ),
              Expanded(
+                flex: 3,
                 child: 
                 Row(                
                 children: <Widget>[
-                  Expanded(child: Container(color: Colors.teal, margin: EdgeInsets.only(left: 8, top: 4, right: 4, bottom: 8),)),
-                  Expanded(child: Container(color: Colors.grey, margin: EdgeInsets.only(left: 4, top: 4, right: 8, bottom: 8),)),
+                  Expanded(child: Container(color: Colors.teal, margin: const EdgeInsets.only(left: 8, top: 4, right: 4, bottom: 8),
+                  child: const Center(child: Text(style: TextStyle(color: Color.fromRGBO(5, 79, 12, 0.965), fontSize: 60.0), '1'),)
+                  )),
+                  Expanded(child: Container(color: Colors.grey, margin: const EdgeInsets.only(left: 4, top: 4, right: 8, bottom: 8),
+                  child: const Center(child: Text(style: TextStyle(color: Color.fromRGBO(123, 63, 8, 0.748), fontSize: 60.0), '1'),)
+                  )),
                   ],
-                  ),              
-                flex: 3,
+                  ),
             ),
           ],
           ),
